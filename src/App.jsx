df5f79d494d44fc46286ded82123b8080bd27840
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-26 09:42:43 +0400

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,67 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect } from "react";
+import { useState } from "react";
+import Loading from "./component/Loading";
+import Tours from "./component/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id);
+    setTours(newTour);
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+
+      setTours(tours);
+
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fetchData()}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 };
 export default App;
---
+++ src/component/Tour.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+      <span className="tour-price">${price}</span>
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}{" "}
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? " ... Show Less" : "Show More"}
+          </button>
+        </p>
+        <button
+          onClick={() => removeTour(id)}
+          type="button "
+          className="btn btn-block delete-btn"
+        >
+          remove
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Tour;
